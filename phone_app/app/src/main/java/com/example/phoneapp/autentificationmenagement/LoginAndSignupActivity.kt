c14d4f0f30a0d4342e96a5276d449bd22815ed57
jss8882@unist.ac.kr <sangsujang@Sangsus-MacBook-Pro.local>
2021-01-07 22:28:54 +0900

    first_commit

---
+++ phone_app/app/src/main/java/com/example/phoneapp/autentificationmenagement/LoginAndSignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.phoneapp.autentificationmenagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phoneapp.R
+
+class LoginAndSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_and_signup)
+    }
+}
