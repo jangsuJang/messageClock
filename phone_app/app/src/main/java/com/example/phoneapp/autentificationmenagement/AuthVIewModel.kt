f27274f576a1f9ac78dd82d49e48e637a2e6b2a9
sangsuJang <jss8882@unist.ac.kr>
2021-01-11 00:01:41 +0900

    signup function

--- phone_app/app/src/main/java/com/example/phoneapp/autentificationmenagement/AuthVIewModel.kt
+++ phone_app/app/src/main/java/com/example/phoneapp/autentificationmenagement/AuthVIewModel.kt
@@ -1,7 +1,78 @@
 package com.example.phoneapp.autentificationmenagement
 
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class AuthViewModel() :ViewModel() {
+
+    private val auth : FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    var user =  MutableLiveData<FirebaseUser?>()
+    var errorCode = MutableLiveData<String?>()
+    val TAG = "AuthViewModel"
+
+
+    fun signup (username:String?, email:String?, password:String?){
+        auth.createUserWithEmailAndPassword(email!!, password!!)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+//                    Toast.makeText(this,"Authentication failed", Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+
+                // ...
+            }
+
+//        auth.createUserWithEmailAndPassword(email!!,password!!)
+//            .addOnCompleteListener { task->
+//                if(task.isSuccessful){
+//                    var tUser = task.result!!.user
+//                    if(tUser != null){
+//                        val profileUpdates = UserProfileChangeRequest.Builder()
+//                            .setDisplayName(username)
+//                            .build()
+//                        tUser.updateProfile(profileUpdates)
+//                            .addOnCompleteListener { task ->
+//                                if (task.isSuccessful) {
+//                                    user.value = tUser
+//                                }
+//                            }
+//                    }
+//                }else{
+//
+//                    val e =
+//                        task.exception as FirebaseAuthException?
+//
+//                    errorCode.value = e!!.errorCode
+//                }
+//            }
+
+    }
+
+    fun isEmailValid(email: String?): Boolean {
+        if (email != null) {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        } else {
+            return false
+        }
+    }
+
+    fun isPasswordValid(password: String?): Boolean {
+        return password?.length!! > 5;
+    }
 
 }
--- phone_app/app/src/main/java/com/example/phoneapp/autentificationmenagement/SignupFragment.kt
+++ phone_app/app/src/main/java/com/example/phoneapp/autentificationmenagement/SignupFragment.kt
@@ -11,6 +11,8 @@ import kotlinx.android.synthetic.main.fragmenet_signup.*
 
 class SignupFragment : Fragment() {
 
+    var loginAndSignupActivity : LoginAndSignupActivity? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,46 +26,48 @@ class SignupFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        loginAndSignupActivity = (activity as LoginAndSignupActivity)
+
         signupBtn.setOnClickListener {
             signupBtn.isCheckable=false
-            var username :String? = signupUsername.editText.toString()
-            var email : String? = signupEmail.editText.toString()
-            var password: String? = signupPassword.editText.toString()
-            var passwordConfirm : String? = signupPasswordConfirm.editText.toString()
+            var username :String? = signupUsername.text.toString()
+            var email : String? = signupEmail.text.toString()
+            var password: String? = signupPassword.text.toString()
+            var passwordConfirm : String? = signupPasswordConfirm.text.toString()
 
-//            var validValue: Boolean  = checkSignupValue(username,email,password,passwordConfirm)
-//            if(!validValue){
-//                signupBtn.isClickable = true
-//            }
-//            else{
-//                loginAndSignupActivity!!.viewModel.signup(userName,email,password,passwordCheck)
-//            }
+            var validValue: Boolean  = checkSignupValue(username,email,password,passwordConfirm)
+            if(!validValue){
+                signupBtn.isClickable = true
+            }
+            else{
+                loginAndSignupActivity!!.viewModel.signup(username,email,password)
+            }
 
         }
 
     }
 
-//    private fun checkSignupValue(userName:String?, email:String?,password : String?, passwordCheck : String?) :Boolean{
-//        if(TextUtils.isEmpty(userName)){
-//            signupUsername.error = "사용자명을 입력해주세요"
-//            return false
-//        }
-////        if(!loginAndSignupActivity!!.viewModel.isPasswordValid(password) || TextUtils.isEmpty(password)){
-////            signupPassword.error = "6자리 이상의 비밀번호를 입력해 주세요."
-////            return false
-////        }
-//        if(passwordCheck != password){
-//            signupPassword.error="비밀번호와 비밀번호 확인이 일치하지 않습니다"
-//            return false
-//        }
-//        if (TextUtils.isEmpty(email)) {
-//            signupEmail!!.error = "이메일 주소를 입력해주세요"
-//            return false
-//        } else if (!loginAndSignupActivity!!.viewModel.isEmailValid(email)) {
-//            signupEmail!!.error = "잘못된 이메일 형식입니다."
-//            return false
-//        }
-//        return true
-//    }
+    private fun checkSignupValue(userName:String?, email:String?,password : String?, passwordCheck : String?) :Boolean{
+        if(TextUtils.isEmpty(userName)){
+            signupUsername.error = "사용자명을 입력해주세요"
+            return false
+        }
+        if(!loginAndSignupActivity!!.viewModel.isPasswordValid(password) || TextUtils.isEmpty(password)){
+            signupPassword.error = "6자리 이상의 비밀번호를 입력해 주세요."
+            return false
+        }
+        if(passwordCheck != password){
+            signupPassword.error="비밀번호와 비밀번호 확인이 일치하지 않습니다"
+            return false
+        }
+        if (TextUtils.isEmpty(email)) {
+            signupEmail!!.error = "이메일 주소를 입력해주세요"
+            return false
+        } else if (!loginAndSignupActivity!!.viewModel.isEmailValid(email)) {
+            signupEmail!!.error = "잘못된 이메일 형식입니다."
+            return false
+        }
+        return true
+    }
 
 }
