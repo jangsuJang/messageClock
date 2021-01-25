0ee56052937d00a7ae68db83f562b3b590fdb418
sangsuJang <jss8882@unist.ac.kr>
2021-01-25 15:26:34 +0900

    AuthUI

--- nodeApp/messageclockapp/src/Routes/Auth.js
+++ nodeApp/messageclockapp/src/Routes/Auth.js
@@ -4,7 +4,15 @@ const Auth = () => {
     const [email,setEmail] = useState("");
     const [password, setPassword] = useState("");
     const onChange = (event) =>{
-        console.log(event.target.name)
+        console.log(event.target.name);
+        const {target: {name, value}} = event;
+        if(name==="email"){
+            setEmail(value);
+            console.log(email)
+        }else if(name === "password"){
+            setPassword(value);
+
+        }
     };
     const onSubmit = (event) =>{
         event.preventDefault()
