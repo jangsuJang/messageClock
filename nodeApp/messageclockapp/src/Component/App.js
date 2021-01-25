30ad367cdd6c9e137ff4002db6f7ea61f07c241b
sangsuJang <jss8882@unist.ac.kr>
2021-01-25 13:15:26 +0900

    auth

--- nodeApp/messageclockapp/src/Component/App.js
+++ nodeApp/messageclockapp/src/Component/App.js
@@ -1,34 +1,23 @@
-import React, {Component} from "react";
+import React,{useState,Component} from "react";
 import { ThemeProvider } from "styled-components";
 import GlobasStyles from "../Styles/GlobasStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
-import firebase from "firebase"
-import firebaseConfig from "../Firebase/firebase.config"
+import firebaseAuth, { authService } from "../Firebase/firebaseAuth"
 
-firebase.initializeApp(firebaseConfig)
+// firebase.initializeApp(firebaseConfig)
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobasStyles/>
+        <AppRouter isLoggedIn={isLoggedIn}/>
+        <footer>&copy; messageClock {new Date().getFullYear()}</footer>
+      </>
+    </ThemeProvider>
+  )
+}
 
-// firebase.auth().signInWithEmailAndPassword("jss8882@naver.com", "wkdtkdtn100")
-//         .then((user) =>{
-//           console.log(user)
-//         })
-//         .catch(function(error) {
-//       // Handle Errors here.
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       if (errorCode === 'auth/wrong-password') {
-//         alert('Wrong password.');
-//       } else {
-//         alert(errorMessage);
-//       }
-//       console.log(error);
-//     });
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobasStyles/>
-      <AppRouter isLoggedIn={true}/>
-    </>
-  </ThemeProvider>
-)
+export default App;
--- nodeApp/messageclockapp/src/Firebase/firebaseAuth.js
+++ nodeApp/messageclockapp/src/Firebase/firebaseAuth.js
@@ -1,12 +1,24 @@
-const handleForm = e => {
-    e.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        if (res.user) Auth.setLoggedIn(true);
-      })
-      .catch(e => {
-        setErrors(e.message);
-      });
-  };
+import firebase from "firebase/app"
+import "firebase/auth"
+import firebaseConfig from "../Firebase/firebase.config"
+
+firebase.initializeApp(firebaseConfig)
+
+// firebase.auth().signInWithEmailAndPassword("jss8882@naver.com", "wkdtkdtn100")
+//           .then((user) =>{
+//             console.log(user)
+//           })
+//           .catch(function(error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         if (errorCode === 'auth/wrong-password') {
+//           alert('Wrong password.');
+//         } else {
+//           alert(errorMessage);
+//         }
+//         console.log(error);
+//       });
+
+export const authService = firebase.auth();
+
